fec44d0c93c81f4d20a36383528ae8092e8097c6
@DAkbari <danial_akbari33@hotmail.com>
2018-09-13 10:16:52 +0430

    before persian changes

--- SPL/menu/views.py
+++ SPL/menu/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def menu(request):
+    if request.methode == 'GET':
+        return HttpResponse('<h1>welcome</h1>')
